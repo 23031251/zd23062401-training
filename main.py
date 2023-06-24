c44b02fcaf5fb1e6c6a73c730dc5d0a2ca2bf5bd
23031251 <zd230219@gmail.com>
2023-06-24 18:52:57 +0700

    23062204 exercise

---
+++ main.py
@@ -0,0 +1,20 @@
+#по 23062202
+# from fastapi import FastAPI
+# from fastapi.responses import HTMLResponse
+# app = FastAPI()
+# @app.get("/")
+# def read_root():
+#     html_content = "<h2>Hello METANIT.COM!!</h2>"
+#     return HTMLResponse(content=html_content)
+
+#по 23062203
+# from fastapi import FastAPI
+# app = FastAPI()
+# @app.get("/")
+# def root():
+#     return {"message": "Hello METANIT.COM"}
+# @app.get("/about")
+# def about():
+#     return {"message": "О сайте"}
+
+# пример 23062204
